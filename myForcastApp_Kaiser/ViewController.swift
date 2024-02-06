ef949d344c2f6e79d4972a901fdd4da3d67710aa
Andrew Kaiser <Andrewkaiser2006@gmail.com>
2024-02-06 09:48:44 -0600

    ?

--- myForcastApp_Kaiser/ViewController.swift
+++ myForcastApp_Kaiser/ViewController.swift
@@ -45,7 +45,9 @@ struct DataForDay{
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,UIPickerViewDelegate,UIPickerViewDataSource {
+   
+    
     // labels
     @IBOutlet weak var dateLabel: UILabel!
     @IBOutlet weak var tempLabel: UILabel!
@@ -55,16 +57,22 @@ class ViewController: UIViewController {
     @IBOutlet weak var humidityLabel: UILabel!
     
     @IBOutlet weak var ImageOutlet: UIImageView!
+    @IBOutlet weak var pickerOutlet: UIPickerView!
     
     //
     
     var fiveDaysData: [DataForDay] = []
     var index = 0
-
+    // New York City, Chicago, Los Angeles, San Fransisco, Miami
+    var majorCities:[String:String] = ["40.71":"-74.00","41.87":"87.62","34.05":"-118.24","37.77":"-122.41","25.76":"-80.19"]
+    var majorCitiesNames = ["New York City","Chicago","Los Angeles","San Fransisco","Miami"]
+    var majorCityIndex = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        pickerOutlet.delegate = self
         // Do any additional setup after loading the view.
-        fiveDay()
+        fiveDay(key: "42.24",value: "-88.31")
         sleep(1)
         dateLabel.text = "Date: \(fiveDaysData[index].dayDate)"
         tempLabel.text = "Temp: \(String(fiveDaysData[index].dayTemp))F°"
@@ -76,11 +84,11 @@ class ViewController: UIViewController {
        
     }
     
-    func fiveDay(){
+    func fiveDay(key: String,value: String){
         print("func running")
         let session = URLSession.shared
 
-                let weatherURL = URL(string: "http://api.openweathermap.org/data/2.5/forecast?lat=42.24&lon=-88.31&cnt=40&units=imperial&appid=524bb8ef5d193608e792fb9733ac8f8f")!
+                let weatherURL = URL(string: "http://api.openweathermap.org/data/2.5/forecast?lat=\(key)&lon=\(value)&cnt=40&units=imperial&appid=524bb8ef5d193608e792fb9733ac8f8f")!
 
                 let dataTask = session.dataTask(with: weatherURL) {
                     (data: Data?, response: URLResponse?, error: Error?) in
@@ -90,6 +98,7 @@ class ViewController: UIViewController {
                     } else {
                         if let data = data {
                             if let jsonObj = try? JSONSerialization.jsonObject(with: data, options: .allowFragments) as? NSDictionary {
+                                print("=====================================")
                                 print(jsonObj)
                                 self.decodeJSONForcast(JSONdata: data)
                                 
@@ -119,7 +128,6 @@ class ViewController: UIViewController {
            // let tempDesc = i.weather.description
             fiveDaysData.append(DataForDay(dayDate: tempDate, dayTemp: tempTemp, dayFeels_like: tempFeels_Like, dayTemp_max: tempDay_max, dayTemp_min: tempDay_min, dayHumidity: tempHumidity))
         }
-        print(fiveDaysData)
     }
     
     @IBAction func forwardAction(_ sender: Any) {
@@ -161,6 +169,45 @@ class ViewController: UIViewController {
         default:
             return "sun.max.fill"
         }
+    }
+    
+   // num of wheels
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    // rows within a wheel
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        if component == 0 {
+            return majorCities.count
+        }
+        return 0
+    }
+    // puts a title in each row
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        if component == 0 {
+            return majorCitiesNames[row]
+        }
+        return ""
+    }
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        if component == 0 {
+            majorCityIndex = row
+        }
+    }
+    
+    @IBAction func submitAction(_ sender: Any) {
+        fiveDaysData.removeAll()
+        let lat = Array(majorCities.keys)[majorCityIndex]
+        let long = Array(majorCities.values)[majorCityIndex]
+        
+        fiveDay(key: lat, value: long)
+        sleep(1)
+        dateLabel.text = "Date: \(fiveDaysData[index].dayDate)"
+        tempLabel.text = "Temp: \(String(fiveDaysData[index].dayTemp))F°"
+        feels_likeLabel.text = "Feels Like: \(String(fiveDaysData[index].dayFeels_like))F°"
+        temp_maxLabel.text = "Max Temp: \(String(fiveDaysData[index].dayTemp_max))F°"
+        temp_minLabel.text = "Min Temp: \(String(fiveDaysData[index].dayTemp_min))F°"
+        humidityLabel.text = "Humidity: \(String(fiveDaysData[index].dayHumidity))%"
     }
     
 }
